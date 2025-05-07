b787c3f5a1c54da65599d35a235d530dcb4b3855
Kynan Sorochan <kynan.soro@outlook.com>
2025-05-07 11:11:29 -0600

    You know the drill

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: resolve(__dirname, 'index.html')
       },
       output: {
         manualChunks: undefined,
